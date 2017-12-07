a03f8a73f61acca8757977aaab3a48de052c1407
osamu yamakami <osamuyamakami@osamunoMacBook-puro.local>
2017-12-07 09:55:56 +0900

    starter install

---
+++ approot/resources/views/develop/list.blade.php
@@ -0,0 +1,11 @@
+<h1>cliche</h1>
+<ul>
+    <li><a href="/list">List</a></li>
+</ul>
+
+<h2></h2>
+<ul>
+    <li><a href="/bootstra3/index">Bootstrap3 defaut</a></li>
+    <li><a href=""></a></li>
+    <li><a href=""></a></li>
+</ul>
--- approot/routes/web.php
+++ approot/routes/web.php
@@ -14,3 +14,18 @@
 Route::get('/', function () {
     return view('welcome');
 });
+
+/**
+ * Develop page
+ *
+ * 2017-12-06
+ * by cliche
+ */
+if (env("APP_ENV")=="local") {
+    /* Develop index */
+    Route::get('/list', function () { return view('develop.list');});
+
+    /* Bootstrap3 */
+    Route::get('/bootstra3/index', function () { return view('bootstrap3.index');});
+}
+
