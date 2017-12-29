9ed2bf4eaeb8446e0e73430b60a78a94ca4256e1
osamuya <yamakami@3kaku.co.jp>
2017-12-29 13:25:59 +0900

    日本語

--- approot/resources/views/auth/login.blade.php
+++ approot/resources/views/auth/login.blade.php
@@ -1,10 +1,11 @@
-@extends('layouts.app')
+@inject('parameter', 'App\Http\Controllers\SetParameter')
+@extends('layouts.applayout')
 
 @section('content')
 <div class="container">
     <div class="row">
         <div class="col-md-8 col-md-offset-2">
-            <div class="panel panel-default">
+            <div class="panel panel-default panel-basic mt60">
                 <div class="panel-heading">Login</div>
 
                 <div class="panel-body">
@@ -12,7 +13,7 @@
                         {{ csrf_field() }}
 
                         <div class="form-group{{ $errors->has('email') ? ' has-error' : '' }}">
-                            <label for="email" class="col-md-4 control-label">E-Mail Address</label>
+                            <label for="email" class="col-md-4 control-label">Eメールアドレス</label>
 
                             <div class="col-md-6">
                                 <input id="email" type="email" class="form-control" name="email" value="{{ old('email') }}" required autofocus>
@@ -26,7 +27,7 @@
                         </div>
 
                         <div class="form-group{{ $errors->has('password') ? ' has-error' : '' }}">
-                            <label for="password" class="col-md-4 control-label">Password</label>
+                            <label for="password" class="col-md-4 control-label">パスワード</label>
 
                             <div class="col-md-6">
                                 <input id="password" type="password" class="form-control" name="password" required>
@@ -43,7 +44,7 @@
                             <div class="col-md-6 col-md-offset-4">
                                 <div class="checkbox">
                                     <label>
-                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> Remember Me
+                                        <input type="checkbox" name="remember" {{ old('remember') ? 'checked' : '' }}> ログイン情報を記憶させる
                                     </label>
                                 </div>
                             </div>
@@ -52,7 +53,7 @@
                         <div class="form-group">
                             <div class="col-md-8 col-md-offset-4">
                                 <button type="submit" class="btn btn-primary">
-                                    Login
+                                    ログイン
                                 </button>
 
                                 <a class="btn btn-link" href="{{ route('password.request') }}">
