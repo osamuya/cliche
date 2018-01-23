5c5315b4b7484afbfdf3d16f8a246a53033ce13a
osamuya <yamakami@3kaku.co.jp>
2018-01-23 23:29:05 +0900

    ES6の書き方習った

---
+++ approot/public/prototype.php
@@ -0,0 +1,59 @@
+
+<!doctype html>
+<!--[if IE 8]> <html class="ie8"> <![endif]-->
+<!--[if IE 9]> <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html lang="ja">
+<!--<![endif]-->
+<head>
+	<meta charset="utf-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+	<title>cliche</title>
+	<meta name="description" content="">
+	<meta name="keywords" content="Laravel">
+	<meta name="csrf-token" content="">
+	<meta property="og:type" content="website">
+	<meta property="og:url" content="http://cliche.local/">
+	<meta property="og:image" content="">
+	<meta property="og:title" content="cliche">
+	<meta property="og:description" content="">
+	<meta property="og:site_name" content="">
+	<link rel="shortcut icon" href=""/>
+	<!--Bootstrap-->
+	<script src="/assets/js/jquery-1.11.3.min.js"></script>
+    <link href="https://maxcdn.bootstrapcdn.com/font-awesome/4.7.0/css/font-awesome.min.css" rel="stylesheet" crossorigin="anonymous">
+    <script type="text/javascript" src="/assets/js/bootstrap.min.js"></script>
+	<!--css-->
+	<link rel="stylesheet" type="text/css" href="/assets/css/style.css" media="all">
+	<!--js-->
+	<script type="text/javascript" src="/assets/js/bundle.js"></script>
+	<!--[if lt IE 10]>
+	<script type="text/javascript" src="assets/js/html5shiv.min.js"></script>
+	<script type="text/javascript" src="assets/js/respond.min.js"></script>
+	<script type="text/javascript" src="assets/js/flexibility.js"></script>
+	<![endif]-->
+    
+    <script>
+        var obj = {name: "chinpo"};
+        console.log(obj);
+
+        var obj = new Object();
+        obj.name = "chinpo";
+        console.log(obj);
+
+        if (obj.__proto__ === Object.prototype) {
+            console.log("true");
+        }
+        
+        console.log(obj.__proto__);
+        console.log(Object.prototype);
+    
+    </script>
+</head>
+<body>
+    
+    
+    
+    
+</body>
+</html>
