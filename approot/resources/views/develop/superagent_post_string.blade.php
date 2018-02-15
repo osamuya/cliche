2c32a92784d22fe01606b619eac20102e2d0b2e9
osamuya <yamakami@3kaku.co.jp>
2018-02-15 10:52:43 +0900

    superAgent get/post 練習

---
+++ approot/resources/views/develop/superagent_post_string.blade.php
@@ -0,0 +1,37 @@
+<!doctype html>
+<!--[if IE 6]> <html class="ie6"> <![endif]-->
+<!--[if IE 7]> <html class="ie7"> <![endif]-->
+<!--[if IE 8]> <html class="ie8"> <![endif]-->
+<!--[if IE 9]> <html class="ie9"> <![endif]-->
+<!--[if gt IE 9]><!-->
+<html lang="ja">
+    <!--<![endif]-->
+    <head>
+        <meta charset="utf-8">
+        <meta name="viewport" content="width=device-width">
+        <!--page info-->
+        <title>sample</title>
+        <meta name="description" content="" />
+        <meta name="keywords" content="" />
+        <!--og-->
+        <meta property="og:type" content="website" />
+        <meta property="og:url" content="http://example.com/" />
+        <meta property="og:title" content="sample" />
+        <meta property="og:description" content="" />
+        <meta property="og:site_name" content="sample" />
+		<!--Twitter-->
+        <meta name="twitter:card" content="summary" />
+        <meta name="twitter:title" content="" />
+        <meta name="twitter:description" content="" />
+        <!--css-->
+        <script src="https://code.jquery.com/jquery-1.12.4.min.js" integrity="sha256-ZosEbRLbNQzLpnKIkEdrPv7lOy9C27hHQ+Xp8a4MxAQ=" crossorigin="anonymous"></script>
+        <!--js-->
+        <!--[if lt IE 10]>
+        <script type="text/javascript" src="/assets/js/plugin/html5shiv.min.js"></script>
+        <script type="text/javascript" src="/assets/js/plugin/respond.min.js"></script>
+        <![endif]-->
+    </head>
+    <body>
+        <h1>Superagent post string</h1>
+    </body>
+</html>
